7182607089110efc09b0d9c403238bba69163d93
pathomphonSia <siatrakool.patomporn@gmail.com>
2021-02-03 16:10:45 +0700

    edit path routerLink

--- shop/src/app/app-routing.module.ts
+++ shop/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { SellProductsComponent } from './sell-products/sell-products.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  // { path: '', redirectTo: 'Home'},
+  { path: 'home-component', component: HomeComponent },
+  { path: 'about-component', component: AboutComponent },
+  { path: 'shop-component', component: ShopComponent },
+  { path: 'sellProducts-component', component: SellProductsComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
