85376605c798a73dcc05cdb199333ba469055f0d
teem <theeranatsre@gmail.com>
2021-02-05 02:27:40 +0700

    edit shopping-cart(ไส่ฟังชั้นคำนวนราคา และเลือกจำนวนสินค้า)

--- shop/src/app/shopping-cart/shopping-cart.component.ts
+++ shop/src/app/shopping-cart/shopping-cart.component.ts
@@ -13,15 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  price : number = 229;
-  pieces : number = 15;
+  price : number = 229.25;
+  pieces : number = 1;
   total_price : number = 0;
   value_check: boolean = false;
   shipping_cost : number = 25;
+  all_total_price: number = 0.00;
+  check: boolean = false;
 
   
   constructor() { 
     this.total_price=this.total(this.price, this.pieces);
+    this.all_total_price=this.all_total(this.price, this.pieces, this.shipping_cost);
+    
   }
 
   ngOnInit(): void {
@@ -42,6 +46,52 @@ export class ShoppingCartComponent implements OnInit {
     const sum =(price*pieces);
     return sum;
   }
+
+  all_total(price:number, pieces:number, shipping_cost:number){
+    sum:Number;
+    const sum =((price*pieces)+shipping_cost);
+    return sum;
+  }
+  
+  onKey(value: string) {
+      this.pieces = Number(value);
+      this.update(); 
+  }
+
+  update(){
+    this.pieces_check(this.pieces);
+    this.total_price=this.total(this.price, this.pieces);
+    this.all_total_price=this.all_total(this.price, this.pieces, this.shipping_cost);
+  }
+
+  onclick_plus(value: string){
+    this.pieces=(Number(value)+1);
+    this.update(); 
+  }
+
+  onclick_minus(value: string){
+    this.pieces=(Number(value)-1);
+    this.update(); 
+  }
+  
+  pieces_check(check:number){
+    if(check <= 0){
+      this.pieces= 1;
+    }else{
+      this.pieces= check;
+    }
+  }
+
+  box_check(e:any, c:boolean){
+    if(e.target.checked){
+      this.check = true;
+    }else{
+      this.check = false; 
+    }
+    return this.check;
+  }
+
+  
   
 
 }
