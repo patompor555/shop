16a1e9d56ba2903fac19f0b4acad542c8f958ea8
pathomphonSia <siatrakool.patomporn@gmail.com>
2021-02-07 13:38:52 +0700

    chang firebase

--- shop/src/environments/environment.ts
+++ shop/src/environments/environment.ts
@@ -5,14 +5,22 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCgapHmBZSjfvirpkDhbYXphAr8Om4pYWc",
-    authDomain: "shop1-932d0.firebaseapp.com",
-    databaseURL: "https://shop1-932d0-default-rtdb.firebaseio.com",
-    projectId: "shop1-932d0",
-    storageBucket: "shop1-932d0.appspot.com",
-    messagingSenderId: "118455595977",
-    appId: "1:118455595977:web:91d8eeb6b9400a6f012e18",
-    measurementId: "G-LS8BDFMM79"
+    // apiKey: "AIzaSyCgapHmBZSjfvirpkDhbYXphAr8Om4pYWc",
+    // authDomain: "shop1-932d0.firebaseapp.com",
+    // databaseURL: "https://shop1-932d0-default-rtdb.firebaseio.com",
+    // projectId: "shop1-932d0",
+    // storageBucket: "shop1-932d0.appspot.com",
+    // messagingSenderId: "118455595977",
+    // appId: "1:118455595977:web:91d8eeb6b9400a6f012e18",
+    // measurementId: "G-LS8BDFMM79"
+    apiKey: "AIzaSyC0G7V0REDrZvRM9HgxbEXi-UsW5rvF03o",
+    authDomain: "test-firebase-realtime-web.firebaseapp.com",
+    databaseURL: "https://test-firebase-realtime-web-default-rtdb.firebaseio.com",
+    projectId: "test-firebase-realtime-web",
+    storageBucket: "test-firebase-realtime-web.appspot.com",
+    messagingSenderId: "442398714466",
+    appId: "1:442398714466:web:913ae925d845d6237f3f3b",
+    measurementId: "G-Q7LRCHPBKM"
   }
 };
 
